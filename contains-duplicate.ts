b8e449ed942770b72539a68ddfbb3f212edc4d0b
JuanBaut <deserd@protonmail.com>
2024-07-20 23:50:35 -0500

    repo init

---
+++ contains-duplicate.ts
@@ -0,0 +1,18 @@
+function containsDuplicate(nums: number[]): any {
+  let stack: number[] = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      stack = [...nums];
+      stack.splice(i, 1);
+
+      if (nums[i] === stack[j]) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+console.log(containsDuplicate([1, 2, 3, 1]));
